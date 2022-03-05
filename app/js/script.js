515980c259f174158fd8180c322cbec1a651da29
WEEZOOKARP <76672483+WEEZOOKARP@users.noreply.github.com>
2022-03-05 13:37:14 +1300

    Update script.js

--- app/js/script.js
+++ app/js/script.js
@@ -1,3 +1,1 @@
-console.log("Why are you even looking here?");
-console.log("Are you trying to find something?");
-console.log("If you had to marry an OS would it be a linux distro or windows?");
+console.log("Orewa College Anarchy");
